303f1f4a6a483ed4f6137f0b270fb93af012f524
p1507706 <kevin.burdin@etu.univ-lyon1.fr>
2020-11-23 14:01:33 +0100

    ajout persistance + lacunarity + octaves

--- ProceduralLandscape/Assets/Scripts/Landscape.cs
+++ ProceduralLandscape/Assets/Scripts/Landscape.cs
@@ -7,12 +7,15 @@ public class Landscape : MonoBehaviour
     public int width;
     public int height;
     public float scale;
+    public int octaves;
+    public float persistence;
+    public float lacunarity;
 
     public bool autoUpdate;
 
     public void Generate()
     {
-        float[,] heightMap = Util.CreatePerlinNoiseMap(width, height, scale);
+        float[,] heightMap = Util.CreatePerlinNoiseMap(width, height, scale, octaves, persistence, lacunarity);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawMap(heightMap);
--- ProceduralLandscape/Assets/Scripts/Util.cs
+++ ProceduralLandscape/Assets/Scripts/Util.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Util
 {
-    public static float[,] CreatePerlinNoiseMap(int width, int height,float scale)
+    public static float[,] CreatePerlinNoiseMap(int width, int height,float scale, int octaves = 0, float persistence = 1, float lacunarity = 1)
     {
         float[,] heightMap = new float[width, height];
 
@@ -12,13 +12,47 @@ public static class Util
         {
             scale = 0.0001f;
         }
+
+        float minHeight = float.MaxValue;
+        float maxHeight = float.MinValue;
+
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                float Scalei = i / scale;
-                float Scalej = j / scale;
-                heightMap[i, j] = Mathf.PerlinNoise(Scalei, Scalej);
+                float amplitude = 1;
+                float frequency = 1;
+
+                float noiseHeight = 0;
+
+                for (int k = 0; k < octaves; k++)
+                {
+                    float Scalei = i / scale * frequency;
+                    float Scalej = j / scale * frequency;
+                    float perlinValue = Mathf.PerlinNoise(Scalei, Scalej) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+                    
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxHeight)
+                {
+                    maxHeight = noiseHeight;
+                } else if (noiseHeight < minHeight)
+                {
+                    minHeight = noiseHeight;
+                }
+                heightMap[i, j] = noiseHeight;
+            }
+        }
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                heightMap[i, j] = Mathf.InverseLerp(minHeight, maxHeight, heightMap[i, j]);
             }
         }
         return heightMap;
